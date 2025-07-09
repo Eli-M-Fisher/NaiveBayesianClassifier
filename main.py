c54f0b36d88ce476ab96bf89be6fca5d9f656cc2
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-07-09 10:36:00 +0300

    Minor update and cleanup to main file

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-from core.controller import Controller
 from ui.user_interface import UserInterface
 
-
 def main():
-    # Path to your phishing CSV dataset
-    file_path = "data/phishing.csv"
-
-    controller = Controller(file_path)
-    ui = UserInterface(controller)
+    ui = UserInterface()
     ui.run()
-
 
 if __name__ == "__main__":
     main()
